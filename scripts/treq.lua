b24e27fe4d5297f1a19ae3231fc59cb2e1e8f500
Tomas Machalek <tomas@machalek.xyz>
2025-05-05 12:34:55 +0200

    Update go version

--- scripts/treq.lua
+++ scripts/treq.lua
@@ -2,8 +2,8 @@ function preprocess (input_rec, buffer)
     return {input_rec}
 end
 
-function transform (input_rec)
-    local out = transform_default(input_rec, 0)
+function transform (input_rec, tz_shift)
+    local out = transform_default(input_rec, tz_shift)
     if input_rec.IsWebApp then
         set_out_prop(out, "IsAPI", false)
     else
